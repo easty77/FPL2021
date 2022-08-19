6bde32145a91a248db1cba3adeb22abb56a2279b
Simon East <simon.east@eneconsulting.be>
2022-08-19 10:35:23 +0200

    Admin urls changed to 2022

--- src/components/FPL2021Admin.js
+++ src/components/FPL2021Admin.js
@@ -18,9 +18,9 @@ const FPL2021Admin = ({predictorId}) => {
   return (
     <>
     <h1>Admin</h1>
-    <a href="/FPL/servlet/ENEFPLServlet?action=download_fpl_fixtures&year=2021" target="_blank">Fixtures</a><br />
-    <a href="/FPL/servlet/ENEFPLServlet?action=download_fpl_bootstrap&year=2021" target="_blank">Bootstrap</a><br />
-    <a href="/FPL/servlet/ENEFPLServlet?action=update_predictions_score&output=json&year=2021" target="_blank">Predictions</a><br />
+    <a href="/FPL/servlet/ENEFPLServlet?action=download_fpl_fixtures&year=2022" target="_blank">Fixtures</a><br />
+    <a href="/FPL/servlet/ENEFPLServlet?action=download_fpl_bootstrap&year=2022" target="_blank">Bootstrap</a><br />
+    <a href="/FPL/servlet/ENEFPLServlet?action=update_predictions_score&output=json&year=2022" target="_blank">Predictions</a><br />
     </>
   );
 };
